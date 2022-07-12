a36c3f19c7ef71e965496c05a912218efe8b0e3d
ariwardana28 <36886965+ariwardana28@users.noreply.github.com>
2022-07-13 01:32:04 +0800

    q

---
+++ skripsi_dea (3).sql
@@ -0,0 +1,420 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2022 at 07:31 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `skripsi_dea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asuhan_bidan`
+--
+
+CREATE TABLE `asuhan_bidan` (
+  `id` int(11) NOT NULL,
+  `riwayat_mensturasi` varchar(255) DEFAULT NULL,
+  `penyakit_terdahulu` varchar(255) DEFAULT NULL,
+  `penyakit_keluarga` varchar(255) DEFAULT NULL,
+  `riwayat_ginekeologi` varchar(255) DEFAULT NULL,
+  `kb` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catatan_persalinan`
+--
+
+CREATE TABLE `catatan_persalinan` (
+  `id` int(50) NOT NULL,
+  `tgl` int(50) DEFAULT NULL,
+  `persalinan` varchar(255) DEFAULT NULL,
+  `terapi` varchar(255) DEFAULT NULL,
+  `penolong` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ctt_perawat`
+--
+
+CREATE TABLE `ctt_perawat` (
+  `id` int(50) NOT NULL,
+  `id_user` int(50) DEFAULT NULL,
+  `soap` text DEFAULT NULL,
+  `id_perawat` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `ver` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ctt_perawat`
+--
+
+INSERT INTO `ctt_perawat` (`id`, `id_user`, `soap`, `id_perawat`, `status`, `ver`, `created_at`, `updated_at`) VALUES
+(1, 2, 'Aman', 3, 1, 2, '2022-06-27 10:56:09', '2022-07-03 18:46:57'),
+(2, 2, 'asam lambung', 3, 0, 1, '2022-06-28 03:10:33', '2022-07-03 18:46:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kartu_rawat_tinggal`
+--
+
+CREATE TABLE `kartu_rawat_tinggal` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `pekerjaan` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `e_ktp` varchar(255) NOT NULL,
+  `diagnosa` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lab`
+--
+
+CREATE TABLE `lab` (
+  `id` int(11) NOT NULL,
+  `id_ctt_perawat` int(11) NOT NULL,
+  `file` varchar(50) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rawat_tinggal`
+--
+
+CREATE TABLE `rawat_tinggal` (
+  `id` int(11) NOT NULL,
+  `diagnosa` varchar(255) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `anamnesa` varchar(255) DEFAULT NULL,
+  `pemeriksaan_fisik` varchar(255) DEFAULT NULL,
+  `infus` varchar(255) DEFAULT NULL,
+  `injeksi` varchar(255) DEFAULT NULL,
+  `ttd_vital` varchar(255) DEFAULT NULL,
+  `id_ctt` varchar(255) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `lab` varchar(225) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rawat_tinggal`
+--
+
+INSERT INTO `rawat_tinggal` (`id`, `diagnosa`, `id_user`, `anamnesa`, `pemeriksaan_fisik`, `infus`, `injeksi`, `ttd_vital`, `id_ctt`, `status`, `lab`, `created_at`, `updated_at`) VALUES
+(1, 'Vertigo', 6, 'sa', '<p>Kepala/Leher : 20</p>\r\n<p>Thorax : 21</p>\r\n<p>Abdomen : 21</p>\r\n<p>Ekstremitas : 21</p>', 'sa', NULL, '<p>TD : 20</p>\r\n<p>Nadi : 20</p>\r\n<p>RR : 20</p>\r\n<p>Suhu :20</p>', '1', 2, '1656895956_post10.jpg', '2022-07-02 11:00:56', '2022-07-03 17:02:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `riwayat_kehamilan`
+--
+
+CREATE TABLE `riwayat_kehamilan` (
+  `id` int(50) NOT NULL,
+  `thn` int(50) DEFAULT NULL,
+  `tempat` varchar(255) DEFAULT NULL,
+  `umur` int(50) DEFAULT NULL,
+  `jenis` varchar(255) DEFAULT NULL,
+  `penolong` varchar(255) DEFAULT NULL,
+  `penyulit` varchar(255) DEFAULT NULL,
+  `jenis_kelamin` varchar(255) DEFAULT NULL,
+  `bb` varchar(255) DEFAULT NULL,
+  `hidup` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `tgl_lahir` int(11) DEFAULT NULL,
+  `tempat_lahir` varchar(255) DEFAULT NULL,
+  `pekerjaan` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `nomor_kk` varchar(255) DEFAULT NULL,
+  `hubungan` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `no_kesehatan` varchar(255) DEFAULT NULL,
+  `jenis_kelamin` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_lahir` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tmp_lahir` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `kerja` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `no_kk` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `hubungan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `no_kesehatan` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `jk` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bidang` varchar(225) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` int(11) DEFAULT 0,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `tgl_lahir`, `tmp_lahir`, `kerja`, `alamat`, `no_kk`, `hubungan`, `status`, `no_kesehatan`, `jk`, `bidang`, `role`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 'admin@gmail.com', NULL, '$2y$10$QPQ6DJBviWFr4qk9cKyLJeXwjxmd4VsaJTo0YipcT6Ihou5PnqsEC', NULL, '2022-06-26 04:03:48', '2022-06-26 04:03:48'),
+(2, 'Rian', '2000-02-12', 'Balikpapan', 'Mahasiswa', 'Gunung Bakaran', '46', 'Anak', 'Belum Menikah', '645', 'Laki-laki', NULL, 0, NULL, NULL, NULL, NULL, '2022-06-27 10:24:56', '2022-06-27 10:24:56'),
+(3, 'Dea', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 'perawat@gmail.com', NULL, '$2y$10$QPQ6DJBviWFr4qk9cKyLJeXwjxmd4VsaJTo0YipcT6Ihou5PnqsEC', NULL, NULL, NULL),
+(4, 'sely', '2007-12-31', 'Samarinda', 'Siswa', 'Kampung Jawa', '54', 'Anak', NULL, '45', 'Perempuan', NULL, 0, NULL, NULL, NULL, NULL, '2022-06-28 03:10:07', '2022-06-28 03:10:07'),
+(5, 'Siska', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Perempuan', NULL, 2, 'siska@gmail.com', NULL, '$2y$10$KLCPFyllh5R3CEspweXJYO.9FUtSZj.AfnWLONo3pkQAksAOJzyWe', NULL, '2022-06-29 07:34:02', '2022-06-29 07:34:02'),
+(6, 'Dokter', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Laki-laki', 'Mata', 3, 'dokter@gmail.com', NULL, '$2y$10$nF480XQSt9d.i/qoDrbi1OckR5XqPFObykm3XkuIgDoES6Gp.67Li', NULL, '2022-06-29 07:40:28', '2022-06-29 07:40:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `asuhan_bidan`
+--
+ALTER TABLE `asuhan_bidan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `catatan_persalinan`
+--
+ALTER TABLE `catatan_persalinan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ctt_perawat`
+--
+ALTER TABLE `ctt_perawat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kartu_rawat_tinggal`
+--
+ALTER TABLE `kartu_rawat_tinggal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lab`
+--
+ALTER TABLE `lab`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `rawat_tinggal`
+--
+ALTER TABLE `rawat_tinggal`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `riwayat_kehamilan`
+--
+ALTER TABLE `riwayat_kehamilan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `asuhan_bidan`
+--
+ALTER TABLE `asuhan_bidan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `catatan_persalinan`
+--
+ALTER TABLE `catatan_persalinan`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ctt_perawat`
+--
+ALTER TABLE `ctt_perawat`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kartu_rawat_tinggal`
+--
+ALTER TABLE `kartu_rawat_tinggal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lab`
+--
+ALTER TABLE `lab`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `rawat_tinggal`
+--
+ALTER TABLE `rawat_tinggal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `riwayat_kehamilan`
+--
+ALTER TABLE `riwayat_kehamilan`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
