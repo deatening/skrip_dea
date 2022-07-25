de69ba7422fd95fbdc24facc77eb79c6961b4661
ariwardana28 <36886965+ariwardana28@users.noreply.github.com>
2022-07-26 01:22:37 +0800

    p[

--- app/Http/Controllers/Dokter/CTTController.php
+++ app/Http/Controllers/Dokter/CTTController.php
@@ -27,7 +27,8 @@ class CTTController extends Controller
     }
 
     public function show($id){
-        $rawat = Rawat::find($id);
+        // $rawat = Rawat::find($id);
+        $rawat = Rawat::where('id_ctt',$id)->first();
         return view('dokter.rawat.show',compact('rawat'));
     }
 
