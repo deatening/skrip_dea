aedc4dc717ae69bf17501e0228b3940202d840cf
ariwardana28 <36886965+ariwardana28@users.noreply.github.com>
2022-08-22 05:57:21 +0800

    1

--- resources/views/petugas/ctt_persalinan/lap_cttp.blade.php
+++ resources/views/petugas/ctt_persalinan/lap_cttp.blade.php
@@ -79,6 +79,7 @@
                                     <th>Nama Perawat</th>
                                     <th>Anamnese</th>
                                     <th>Terapi</th>
+                                    <th>Rawat Inap/Jalan</th>
                                     <!-- <th>Nama Bidan</th> -->
                                     <td>Action</td>
                                 </tr>
@@ -96,6 +97,7 @@
                                             <td>{{ $item->Perawat->name }}</td>
                                             <td>{{ $item->persalinan }}</td>
                                             <td>{{ $item->terapi }}</td>
+                                            <td>{{ $item->Asuhan->rj }}</td>
                                             <!-- <td>{{ $item->penolong }}</td> -->
                                             <td>
                                                 @if ($item->status == '0')
